bfc38c731f6aa0797bc3ba2c51a04288839bd813
Ignacio Munoz <itmunoz1@uc.cl>
2022-10-09 20:29:51 -0300

    Casi listo con jwt

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+             controllers: {
+               sessions: 'users/sessions',
+               registrations: 'users/registrations'
+             },
+             defaults: { format: :json }
   resources :admins
   resources :reviews
   resources :rental_agreements
   resources :leaseholders
   resources :lessors
-  resources :users
+  # resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
