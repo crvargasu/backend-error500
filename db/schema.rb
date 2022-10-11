1c29549bb47f65f1fb20365342d34e8ab0669820
crvargasu <48322767+crvargasu@users.noreply.github.com>
2022-10-10 22:15:25 -0300

    fix: db changed and all working

--- db/schema.rb
+++ db/schema.rb
@@ -73,6 +73,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_10_041256) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "name"
+    t.string "picture"
+    t.string "phone"
+    t.string "street"
+    t.string "city"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
