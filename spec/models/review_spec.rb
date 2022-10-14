dbfda97d2a2cab1aae73db50c809751918d78d45
Ignacio Munoz <itmunoz1@uc.cl>
2022-10-14 15:42:53 -0300

    40 fix models by removing api v1 modules

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe Api::V1::Review, type: :model do
+RSpec.describe Review, type: :model do
   describe 'Attributes' do
     it { should respond_to :score }
     it { should respond_to :comment }
-    it { should respond_to :api_v1_leaseholders_id }
+    it { should respond_to :leaseholder_id }
   end
 
   describe 'Associations' do
-    # it { should belong_to(:leaseholder) }
+    it { should belong_to(:leaseholder) }
   end
 end
