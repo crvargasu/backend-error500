71bc4e6d00657b4593dc8f370b1001240546ec17
crvargasu <48322767+crvargasu@users.noreply.github.com>
2022-10-19 21:15:49 -0300

    User information returned when sign in

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,14 +2,32 @@ class Users::SessionsController < Devise::SessionsController
     respond_to :json
     private
     def respond_with(resource, _opts = {})
-      render json: { message: 'Logged.' }, status: :ok
+      current_user ? log_in_success : log_in_failure
     end
+
     def respond_to_on_destroy
       current_user ? log_out_success : log_out_failure
     end
+
+    def log_in_success
+      if(Lessor.where(user_id: current_user.id).exists?)
+        lessor = Lessor.where(user_id: current_user.id)
+        render json: {"user": current_user, "other": lessor[0]}, status: :ok
+      end
+      if(Leaseholder.where(user_id: current_user.id).exists?)
+        leaseholder = Lessor.where(user_id: current_user.id)
+        render json: {"user": current_user, "other": leaseholder[0]}, status: :ok
+      end
+    end
+
+    def log_in_failure
+      render json: { message: "Logged in failure."}, status: :unauthorized
+    end
+
     def log_out_success
       render json: { message: "Logged out." }, status: :ok
     end
+    
     def log_out_failure
       render json: { message: "Logged out failure."}, status: :unauthorized
     end
