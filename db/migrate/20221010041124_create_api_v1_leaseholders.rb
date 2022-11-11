e60379be69505751a248c2bab38ae6df458e668f
crvargasu <48322767+crvargasu@users.noreply.github.com>
2022-11-10 23:04:37 -0300

    Leaseholder formate changed

--- db/migrate/20221010041124_create_api_v1_leaseholders.rb
+++ db/migrate/20221010041124_create_api_v1_leaseholders.rb
@@ -2,7 +2,12 @@ class CreateApiV1Leaseholders < ActiveRecord::Migration[7.0]
   def change
     create_table :leaseholders do |t|
       t.string :property_account
-      t.st_polygon :polygon
+      t.text :description
+      t.integer :capacity
+      t.integer :highlimit
+      t.string :polygon
+      t.integer :area
+      t.string :center
       t.integer :mean_reviews
       t.integer :credit
       t.boolean :status
