ff459592f57ba6ae847d439d953f5d1194a0f753
Ignacio Munoz <itmunoz1@uc.cl>
2022-11-15 09:42:58 -0300

    Remove ApiHelper as it is not used

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -66,7 +66,7 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
-  config.include ApiHelpers
+  # config.include ApiHelpers
 
   config.include Request::AuthHelpers, type: :request
 
