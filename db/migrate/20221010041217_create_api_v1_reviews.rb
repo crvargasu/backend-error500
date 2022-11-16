8c7aeef1bb8559a24808a8a96294be8e4ad38583
crvargasu <48322767+crvargasu@users.noreply.github.com>
2022-11-15 21:00:43 -0300

    fix: strings limited in models

--- db/migrate/20221010041217_create_api_v1_reviews.rb
+++ db/migrate/20221010041217_create_api_v1_reviews.rb
@@ -4,7 +4,7 @@ class CreateApiV1Reviews < ActiveRecord::Migration[7.0]
   def change
     create_table :reviews do |t|
       t.integer :score
-      t.text :comment
+      t.text :comment, :limit => 500
 
       t.timestamps
     end
--- db/migrate/20221105204246_add_fields_to_rental_agreements.rb
+++ db/migrate/20221105204246_add_fields_to_rental_agreements.rb
@@ -2,7 +2,7 @@
 
 class AddFieldsToRentalAgreements < ActiveRecord::Migration[7.0]
   def change
-    add_column :rental_agreements, :reasons, :string
+    add_column :rental_agreements, :reasons, :string, :limit => 100
     add_column :rental_agreements, :offer_price, :decimal
   end
 end
